137db2f502e944283bccdf1a397c6fb6a85a21c0
Кирилл <kkirillcore@gmail.com>
2020-04-03 11:28:45 +0300

    Update 2020-04-03T08:28:42.892Z

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
-"use strict";
+'use strict';
 
-(function() {
-  var pageHeader = document.querySelector(".page-header");
-  var menuToggle = document.querySelector(".js-menu-toggle");
-  var basket = document.querySelector(".basket");
-  var submitButton = basket.querySelector(".js-submit-button");
-  var confirmationCheckbox = basket.querySelector(".js-confirmation");
+(function () {
+  var pageHeader = document.querySelector('.page-header');
+  var menuToggle = document.querySelector('.js-menu-toggle');
+  var basket = document.querySelector('.basket');
+  var submitButton = basket.querySelector('.js-submit-button');
+  var confirmationCheckbox = basket.querySelector('.js-confirmation');
 
-  menuToggle.addEventListener("click", function() {
-    pageHeader.classList.toggle("page-header--menu-closed");
+  menuToggle.addEventListener('click', function () {
+    pageHeader.classList.toggle('page-header--menu-closed');
   });
 
-  var validateCheckbox = function(input) {
+  var validateCheckbox = function (input) {
     if (input.checked === false) {
-      input.setCustomValidity("подтвердите согласие");
-      input.classList.add("basket__confirmation-input--error");
+      input.setCustomValidity('подтвердите согласие');
+      input.classList.add('basket__confirmation-input--error');
       return false;
     } else {
-      input.setCustomValidity("");
-      if (input.classList.contains("basket__confirmation-input--error")) {
-        input.classList.remove("basket__confirmation-input--error");
+      input.setCustomValidity('');
+      if (input.classList.contains('basket__confirmation-input--error')) {
+        input.classList.remove('basket__confirmation-input--error');
       }
       return true;
     }
   };
 
-  submitButton.addEventListener("click", function(evt) {
+  submitButton.addEventListener('click', function (evt) {
     if (validateCheckbox(confirmationCheckbox)) {
       evt.preventDefault();
     }
